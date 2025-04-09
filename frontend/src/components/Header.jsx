bef03adb6b1f7af0ed079f7cd436f98ecfdec986
Luke Cheng <Lukeecheng@gmail.com>
2025-04-09 11:46:04 -0700

    updated header UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import CartIcon from './CartIcon';
-import "./Header.css"
+import './header.css';
 
 export default function Header({ user, onLogout }) {
   const navigate = useNavigate();
@@ -12,20 +12,21 @@ export default function Header({ user, onLogout }) {
 
   return (
     <header>
-      <h1 id='swiftShop'>Swift Shop</h1>
-      {user ? (
-        <>
-          <CartIcon />
-          <Link to='/'>Home</Link>
-          <Link to='/checkout'>Checkout</Link>
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      ) : (
-        <>
-          {/* <Link to='/login'>Login</Link>
-          <Link to='/signup'>Signup</Link> */}
-        </>
-      )}
+      <h1>Swift Shop</h1>
+      <nav>
+        {user ? (
+          <>
+            <CartIcon />
+            <Link to='/checkout'>Checkout</Link>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+          </>
+        )}
+      </nav>
     </header>
   );
 }
